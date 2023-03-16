e33c5b86c048fb5f074e285efe4a8a88cea72e43
spsaikumar <pavansaikumar.s@gmail.com>
2023-03-16 11:58:41 +0100

    Script for POST request to endpoint

--- submit_challenge.py
+++ submit_challenge.py
@@ -6,10 +6,10 @@ url = 'https://dps-challenge.netlify.app/.netlify/functions/api/challenge'
 
 # Define the JSON data
 data = {
-    'github': 'https://github.com/ACCOUNT/REPO',
-    'email': 'EMAIL',
-    'url': 'DEPLOYED_ENDPOINT',
-    'notes': 'NOTES' # Not mandatory
+    'github': 'https://github.com/ACCOUNT/REPO', #https://github.com/spsaikumar/DPS_challenge
+    'email': 'EMAIL', #corework.singavarapu@gmail.com
+    'url': 'DEPLOYED_ENDPOINT',  #https://spsaikumar-dps-challenge-app-swrm06.streamlit.app/
+    'notes': 'NOTES' # Not mandatory 
 }
 
 # Set the headers
