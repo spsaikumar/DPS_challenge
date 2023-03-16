27e03d9c0e4fa02d8783f03db1be00143b7178f0
spsaikumar <pavansaikumar.s@gmail.com>
2023-03-16 11:59:22 +0100

    Script to setup streamlit app creation

--- setup.sh
+++ setup.sh
@@ -1,31 +1,3 @@
-# from setuptools import find_packages,setup
-# from typing import List
-
-# HYPEN_E_DOT='-e .'
-# def get_requirements(file_path:str)->List[str]:
-#     '''
-#     this function will return the list of requirements
-#     '''
-#     requirements=[]
-#     with open(file_path) as file_obj:
-#         requirements=file_obj.readlines()
-#         requirements=[req.replace("\n","") for req in requirements]
-
-#         if HYPEN_E_DOT in requirements:
-#             requirements.remove(HYPEN_E_DOT)
-    
-#     return requirements
-
-# setup(
-# name='DPS_challenge',
-# version='0.0.1',
-# author='Pavan',
-# author_email='corework.singavarapu@gmail.com',
-# packages=find_packages(),
-# install_requires=get_requirements('requirements.txt')
-# )
-
-
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
