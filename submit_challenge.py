3f85e1794f4be3f77f6fedf242da02d1fc5c434f
spsaikumar <pavansaikumar.s@gmail.com>
2023-03-17 08:42:13 +0100

    Script to submit challenge by POST request

--- submit_challenge.py
+++ submit_challenge.py
@@ -6,10 +6,10 @@ url = 'https://dps-challenge.netlify.app/.netlify/functions/api/challenge'
 
 # Define the JSON data
 data = {
-    'github': 'https://github.com/ACCOUNT/REPO', #https://github.com/spsaikumar/DPS_challenge
-    'email': 'EMAIL', #corework.singavarapu@gmail.com
-    'url': 'DEPLOYED_ENDPOINT',  #https://spsaikumar-dps-challenge-app-swrm06.streamlit.app/
-    'notes': 'NOTES' # Not mandatory 
+    "github": "https://github.com/spsaikumar/DPS_challenge",
+    "email": "corework.singavarapu@gmail.com", 
+    "url": "https://spsaikumar-dps-challenge-app-swrm06.streamlit.app/", 
+    "notes": "I apologize for the delay in completing the task. I was sick and had pending exams and seminar deadlines. I am working to complete it. Thank you for your understanding. I am looking forward to see you for next round,interviewer call"
 }
 
 # Set the headers
